2efc25eac592ba6c30874ac603a8da0fb1bfd873
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-09-29 12:36:35 +0200

    ping-pong

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,5 +15,19 @@ public class Main {
 
         hiloPing.start();
         hiloPong.start();
+        try {
+            Semaphore pingSemaphore = new Semaphore(1);
+            Semaphore pongSemaphore = new Semaphore(1);
+
+            pongSemaphore.acquire();
+
+            Thread pingThread = new Thread(new PingTask(pingSemaphore, pongSemaphore));
+            Thread pongThread = new Thread(new PongTask(pingSemaphore, pongSemaphore));
+
+            pingThread.start();
+            pongThread.start();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
