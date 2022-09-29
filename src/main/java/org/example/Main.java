2dfaafad68e9cf7806c56589135fba5fc0f2071a
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-09-29 12:38:52 +0200

    ping-pong

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,6 @@ import java.util.concurrent.Semaphore;
 public class Main {
     public static void main(String[] args){
 
-        Semaphore semaphorePing = new Semaphore(2);
-        Semaphore semaphorePong = new Semaphore(2);
-
-        Thread hiloPing = new Thread(new PingTask(semaphorePing, semaphorePong));
-        Thread hiloPong = new Thread(new PongTask(semaphorePing, semaphorePong));
-
-
-        hiloPing.start();
-        hiloPong.start();
         try {
             Semaphore pingSemaphore = new Semaphore(1);
             Semaphore pongSemaphore = new Semaphore(1);
